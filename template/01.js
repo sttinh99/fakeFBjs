66ea8e8c8a8567566375c442166d5fcf28653f10
sttinh99 <17110381@student.hcmute.edu.vn>
2020-01-12 22:59:17 +0700

    update fbFake

--- template/01.js
+++ template/01.js
@@ -1,10 +1,11 @@
 var anhs = document.querySelectorAll('.cacAnh img');
 var showSlide = document.querySelector('.nenDen');
 var dongTab = document.querySelector('.closeTab');
+var stt = document.querySelectorAll('.khoiCacAnh ul li img');
+var chuyenT = document.querySelector('p.nutTrai');
+var chuyenP = document.querySelector('p.nutPhai');
+var anh = 0;
 dongTab.addEventListener('click',function(){
-    showSlide.classList.remove('hienThi');
-})
-showSlide.addEventListener('click',function(){
     showSlide.classList.remove('hienThi');
 })
 for(var i =0;i<anhs.length;i++)
@@ -13,3 +14,29 @@ for(var i =0;i<anhs.length;i++)
         showSlide.classList.add('hienThi');
     })
 }
+
+chuyenP.addEventListener('click',function(){
+    if(anh >= 0)
+    {
+        for(var i = 0;i<stt.length;i++)
+        {
+            stt[i].classList.remove('hienlen');
+        }
+        anh++;
+        if(anh == stt.length)
+        {
+            anh = 0;
+        }
+        stt[anh].classList.add('hienlen');
+    }
+})
+chuyenT.addEventListener('click',function(){
+    if(anh != 0){
+        for(var i = 0;i<stt.length;i++)
+        {
+            stt[i].classList.remove('hienlen');
+        }
+        anh--;
+        stt[anh].classList.add('hienlen');
+    }
+})
